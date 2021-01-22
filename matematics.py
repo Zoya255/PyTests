52d4cd1d7fb0d33ac6ad88c0ac30003c8e946d6e
Zoya255 <lasramore@gmail.com>
2021-01-22 17:15:10 +1000

    add matematics.py

---
+++ matematics.py
@@ -0,0 +1,53 @@
+
+a = 5
+b = 15
+
+
+def minus( a, b ):
+	print(a - b)
+
+
+def plus( a, b ):
+	print(a + b)
+
+
+def multiply( a, b ):
+	print(a * b)
+
+
+def divide ( a, b ):
+	print(a / b)
+
+
+def test(a, b):
+	if a < b:
+		print( f"{b} is cool")
+	elif a > b:
+		print( f"{a} is cool")
+	else:
+		print( f"{a} and {b} is cool")
+
+
+# тут будет ваша функция с оператором сравнения
+def text_len(text1 = "aaa", text2 = "aab"):
+	text_len1 = len(text1)
+	text_len2 = len(text2)
+
+	if text_len1 < text_len2:
+		print(text2)
+	elif text_len1 > text_len2:
+		print(text1)
+	else:
+		print(text1, text2)
+
+if __name__ == '__main__':
+	minus(a, 1)
+	plus(100, b)
+	multiply(a,b)
+	divide(20, 5)
+	print()
+	test(1, 1)
+
+	print(len("1234567"))
+
+	text_len()
